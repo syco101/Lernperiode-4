a76a16dccdeda082595f867610a3d3eb37488cad
syco101 <142594949+syco101@users.noreply.github.com>
2024-03-22 14:34:04 +0100

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeBot_9000
+{
+    internal class Class3
+    {
+    }
+}
