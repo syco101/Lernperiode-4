10e10f8640d1d36c28996940f5e4e9376d672d11
syco101 <142594949+syco101@users.noreply.github.com>
2024-04-05 14:29:23 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using Discord.WebSocket;
 using System.Security.Cryptography.X509Certificates;
 
-namespace SeBot_9000
+class Program
 {
-    public class Program
+    private DiscordSocketClient _client;
+
+    static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+    public async Task MainAsync()
     {
+        _client = new DiscordSocketClient();
+        _client.Log += LogAsync;
+        _client.MessageReceived += MessageReceivedAsync;
 
+        var token = ; 
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
 
-
+        await Task.Delay(-1);
